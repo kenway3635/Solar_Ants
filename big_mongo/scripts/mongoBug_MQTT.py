a4b1231491aff9a4120be5f1692b857fde153959
croso1024 <croso1024@gmail.com>
2022-07-26 10:29:29 +0800

    0726

--- big_mongo/scripts/mongoBug_MQTT.py
+++ big_mongo/scripts/mongoBug_MQTT.py
@@ -3,16 +3,19 @@ import rospy , json
 import  paho.mqtt.client as mqtt
 from std_msgs.msg  import String,Int64 ,Int16 , Int32, Bool , Float32MultiArray,Float32 
 from geometry_msgs.msg import Twist
-#from ROS_msgParser import * 
+import asyncio 
 from collections import namedtuple 
+
+
 def std_Parser(msg): 
     return  {"data":msg.data} 
 
 def twist_Parser(msg): 
     return {"x":msg.linear.x,"z":msg.angular.z} 
 
-class MongoBug_MQTT(mqtt.Client): 
     
+class MongoBug_MQTT(mqtt.Client): 
+    latch ={} 
     dataClass = {
         "String":String , "Int64":Int64 , "Int16":Int16 , "Int32":Int32 , "Twist":Twist,"Bool":Bool ,
         "Float32MultiArray":Float32MultiArray, "Float32":Float32
@@ -49,26 +52,36 @@ class MongoBug_MQTT(mqtt.Client):
         
         for topic in self.ROSTOPIC : 
             rospy.loginfo(f"Subscribe ROS topic : {topic}")
-
+            
+            self.latch.update({topic["Topic_name"]:None})
+            
             rospy.Subscriber(
                 topic["Topic_name"] , 
                 self.dataClass[topic["data_class"]] , 
-                callback = self._mqttPublish,
-                #callback_args=(topic["Topic_name"],topic["msg_type"]), 
+                #callback = self._mqttPublish,
+                callback = self._updateLatch , 
                 callback_args=self.callbackArg(topic["Topic_name"],topic["msg_type"]),
                 queue_size=topic["quene_size"]
             )
         rospy.loginfo("DataBase register Done! ")
+        rospy.Timer(rospy.Duration(2),self.mqttPublish)
+        
         rospy.spin()
 
-    
+
+    # old version
     def _mqttPublish(self,msg,arg): 
-        #msg = self.msg_type_parser[arg[1]](msg) 
-        #self.publish(arg[0],json.dumps(msg),qos=0) 
-        #print(f"publish {arg.topic}")
+        
         msg = self.msg_type_parser[arg.msgType](msg)
         self.publish(arg.topic,json.dumps(msg),qos=0) 
     
+    def _updateLatch(self,msg,arg):
+        self.latch[arg.topic] = self.msg_type_parser[arg.msgType](msg)
+        
+    def mqttPublish(self,*args): 
+       
+        for key,item in self.latch.items(): 
+            self.publish(key,json.dumps(item),qos=0)
 
 if __name__ == "__main__": 
     
